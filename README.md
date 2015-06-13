a144e2c672ed131f858d49472794b78947ba685b
Archs <gitarchs@yeah.net>
2015-06-13 11:19:21 +0800

    refine identifier and Value order

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Parsing typescript .d.ts file to generate JSON or Gopherjs package automatically
 The parser is created using [PEG for Go](https://github.com/pointlander/peg).
 
 This work is in progress, I'll finish it soon. 
+
+# ATTENTION
+
+1. BOM is not supported
+2. 
--- data/test.d.ts
+++ data/test.d.ts
@@ -228,4 +228,35 @@ declare var ko: KnockoutStatic;
 
 declare module "knockout" {
     export = ko;
+    variables: {
+                clang: number;
+                host_arch: string;
+                node_install_npm: boolean;
+                node_install_waf: boolean;
+                node_prefix: string;
+                node_shared_openssl: boolean;
+                node_shared_v8: boolean;
+                node_shared_zlib: boolean;
+                node_use_dtrace: boolean;
+                node_use_etw: boolean;
+                node_use_openssl: boolean;
+                target_arch: string;
+                v8_no_strict_aliasing: number;
+                v8_use_snapshot: boolean;
+                visibility: string;
+            };
+     memoryUsage(): { rss: number; heapTotal: number; heapUsed: number; };
+     send?(message: any, sendHandle?: any): void;
+     Float32Array: typeof Float32Array;
+    Float64Array: typeof Float64Array;
+    Function: typeof Function;
+    setTimeout: (callback: (...args: any[]) => void, ms: number, ...args: any[]) => NodeJS.Timer;
+    undefined: typeof undefined;
+        unescape: (str: string) => string;
+        gc: () => void;
+    export function parse(str: string, sep?: string, eq?: string, options?: { maxKeys?: number; }): any;
+    emit(event: string, ...args: any[]): boolean;
+    import events = require("events");
+    import net = require("net");
+    import stream = require("stream");
 }
